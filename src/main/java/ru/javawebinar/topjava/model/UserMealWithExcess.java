35cf894925ec8888a38bc4a4fea5aeeae65bcc73
Dmitry Buinovsky <buinovsky.d@gmail.com>
2020-05-22 21:18:42 +0500

    Apply patch prepareToHW0_2

--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
@@ -4,11 +4,8 @@ import java.time.LocalDateTime;
 
 public class UserMealWithExcess {
     private final LocalDateTime dateTime;
-
     private final String description;
-
     private final int calories;
-
     private final boolean excess;
 
     public UserMealWithExcess(LocalDateTime dateTime, String description, int calories, boolean excess) {
